68ad0ffffad1b7e4180ff59738e75de6256ae46d
VirtoOZ <CenTROY@yandex.ru>
2024-08-01 12:41:37 +0300

    favicon + ibg done

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -36,7 +36,8 @@ import { _slideDown } from "./functions.js";
 //SlideToggLe
 import { _slideToggle } from "./functions.js";
 //</ANIM-SLIDE>================================================================
-
+import { iBg } from "./functions.js";
+iBg();
 //<isMobile>================================================================
 //Проверка на каком устройстве работаем
 import { isMobile } from "./functions.js";
