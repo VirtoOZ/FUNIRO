4c167d13a208b77d592799964ab300432b925fa5
VirtoOZ <109903427+VirtoOZ@users.noreply.github.com>
2024-08-02 05:53:14 +0300

    update path

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -7,33 +7,37 @@ const srcFolder = `./src`;
 export const path = {
 	// объект путей с результатом
 	build: {
+		html: `${buildFolder}/`,
 		js: `${buildFolder}/js/`,
 		css: `${buildFolder}/css/`,
-		html: `${buildFolder}/`,
 		images: `${buildFolder}/img/`,
 		fonts: `${buildFolder}/fonts/`,
 		files: `${buildFolder}/files/`,
+		videos: `${buildFolder}/videos/`,
+
 	},
 	// объект путей к исходным файлам
 	src: {
-		js: `${srcFolder}/js/app.js`, //путь к папке c файлом стилей
-		images: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp,ico}`, //путь к папке c файлом стилей
+		favicon: `${srcFolder}/img/favicon.{jpg,jpeg,png,gif,ico,webp}`, //путь к папке c файлом стилей
+		html: `${srcFolder}/*.html`, //путь к папке в которой есть файлы html
+		js: [`${srcFolder}/js/app.js`, `${srcFolder}/js/vendors.js`], //путь к папке c файлом стилей
+		scss: `${srcFolder}/scss/style.scss`, //путь к папке c файлом стилей
+		images: [`${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp,ico}`, `!**/favicon.*`], //путь к папке c файлом стилей
+		fonts: `${srcFolder}/fonts/*.ttf`, //путь к папке которую хотим копировать
 		svg: `${srcFolder}/img/**/*.svg`, //путь к папке c файлом стилей
 		svgicons: `${srcFolder}/svgicons/*.svg`, //путь к папке c файлом стилей
-		scss: `${srcFolder}/scss/style.scss`, //путь к папке c файлом стилей
-		html: `${srcFolder}/*.html`, //путь к папке в которой есть файлы html
 		// html: `${srcFolder}/*.pug`, // Для Pug`, путь к папке в которой есть файлы html
 		files: `${srcFolder}/files/**/*.*`, //путь к папке которую хотим копировать
-		fonts: `${srcFolder}/fonts/*.ttf`, //путь к папке которую хотим копировать
+		videos: `${srcFolder}/videos/*.*`, //путь к папке которую хотим копировать
 	},
 	// объект путей к файлам за которыми нужно следить
 	watch: {
+		html: `${srcFolder}/**/*.html`,
 		js: `${srcFolder}/js/**/*.js`, //путь к файлам которые необходимо отслеживать на изменение
 		scss: `${srcFolder}/scss/**/*.scss`, //путь к файлам которые необходимо отслеживать на изменение
-		html: `${srcFolder}/**/*.html`,
 		// html: `${srcFolder}/**/*.pug`, // Для Pug
-		images: `${srcFolder}/img/**/*.+(jpg|jpeg|png|gif|webp|ico|svg)`, //путь к папке c файлом стилей
-		// images: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp,ico,svg}`, //путь к папке c файлом стилей
+		// images: `${srcFolder}/img/**/*.+(jpg|jpeg|png|gif|webp|ico|svg)`, //путь к папке c файлом стилей
+		images: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp,ico,svg}`, //путь к папке c файлом стилей
 		files: `${srcFolder}/files/**/*.*`, //путь к файлам которые необходимо отслеживать на изменение
 	},
 	clean: buildFolder,
